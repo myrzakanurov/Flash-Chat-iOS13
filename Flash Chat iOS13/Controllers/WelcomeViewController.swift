fe51e390c2aaab4d9111fc302dd9ebc8df1c3cea
Myrzakanurov Arman <myrzakanurov.a@gmail.com>
2021-03-18 18:18:19 +0600

    Completed version

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -12,10 +12,31 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        var index = 0.0
+        
+        if let titleString = titleLabel.text {
+            titleLabel.text = ""
+            
+            for char in titleString {
+                Timer.scheduledTimer(withTimeInterval: 0.1 * index, repeats: false) { (timer) in
+                    self.titleLabel.text?.append(char)
+                }
+                index += 1
+            }
+        }
     }
     
 
---
+++ Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Arman Myrzakanurov on 16.03.2021.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
